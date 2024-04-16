f7667f52327e47dad1c92c9793804054ddaf5151
TheKingEdoardo <edoardobramini@gmail.com>
2024-04-16 17:29:27 +0200

    no message

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ d3.csv("PostiLodi.csv").then(function (data) {
 
     output = _.sortBy(filteredData, 'rating').reverse()
 
-    for (var i = 0; i < filteredMovies.length; i++) {d3.select("tbody").insert("tr").html(
+    for (var i = 0; i < filteredData.length; i++) {d3.select("tbody").insert("tr").html(
       "<td>" + [i+1] + "</td>" +
       "<td>" + (output[i]['name'])+"</a>"+"</td>" + 
       "<td>" + (output[i]['reviews'])+"</td>" +
