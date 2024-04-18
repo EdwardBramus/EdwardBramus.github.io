25767ccdc04e965d7835c9f608a3acb886c4ed91
Gabriele <40129271+Libreage@users.noreply.github.com>
2024-04-18 23:37:52 +0200

    Aggiunta geolocalizzazione + ordinamento codice

---
+++ geolocation.js
@@ -0,0 +1,14 @@
+const x = document.getElementById("geolocation");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocalizzazione non supportata.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitudine: " + position.coords.latitude +
+  "<br>Longitudine: " + position.coords.longitude;
+}
