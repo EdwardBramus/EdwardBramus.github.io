dce5bf10c63cccc8b2931b8d836708ce798a8533
Gabriele <40129271+Libreage@users.noreply.github.com>
2024-04-19 10:57:30 +0200

    Prova geolocalizzazione fake

--- app.js
+++ app.js
@@ -1,61 +1,78 @@
 //Il Modulo D3 carica il csv e dopodichè lo filtra...
 
-d3.csv("PostiLodi.csv").then(function (data) {
+import {
+    getDistanceFromLatLonInKm
+} from './geolocation.js'; // Mi importo la funzione per calcolare la distanza da geolocation
 
-//Definizione delle variabili
+// MOCK PER GEOLOCALIZZAZIONE FAKE
+const stazioneLodiLatitude = 45.309217326050074;
+const stazioneLodiLongitude = 9.497601714830633;
 
-  var PostiLodi = data;
-  var button = d3.select("#button");
-  var form = d3.select("#form");
+d3.csv("PostiLodi.csv").then(function(data) {
 
-//Interazione dell'utente con gli oggetti che fa partire la funzione runEnter
+    //Definizione delle variabili
 
-  button.on("click", runEnter);
-  form.on("submit", runEnter);
+    var PostiLodi = data;
+    var button = d3.select("#button");
+    var form = d3.select("#form");
 
-//Definizione della funzione funEnter
+    //Interazione dell'utente con gli oggetti che fa partire la funzione runEnter
 
-function runEnter() {
+    button.on("click", runEnter);
+    form.on("submit", runEnter);
 
-  d3.select("tbody").html("")
-  d3.selectAll("p").classed('noresults', true).html("")
-  d3.event.preventDefault();
+    //Definizione della funzione runEnter
 
-//Viene preso in entrata l'input dell'utente e poi ripulito nella riga successiva come nuova variabile...
-    
-  var inputElement =  d3.select("#user-input");    
-  var inputValue = inputElement.property("value").toLowerCase().trim();
+    function runEnter() {
 
-//Primo check: l'input dell'utente è troppo breve...
+        d3.select("tbody").html("")
+        d3.selectAll("p").classed('noresults', true).html("")
+        d3.event.preventDefault();
 
-    if (inputValue.length < 3){
-      d3.select("p").classed('noresults2', true).html("<center><strong>Parola troppo corta! Digita almeno 3 lettere!</strong>")
-      inputValue = "Something to give no results"
-    }
+        //Viene preso in entrata l'input dell'utente e poi ripulito nella riga successiva come nuova variabile...
 
-//Qui avviene la magia: filtro in base al nome del locale partendo dall'input utente
+        var inputElement = d3.select("#user-input");
+        var inputValue = inputElement.property("value").toLowerCase().trim();
 
-    var filteredData = PostiLodi.filter(PostiLodi => PostiLodi.categories.toLowerCase().trim().includes(inputValue));
+        //Primo check: l'input dell'utente è troppo breve...
 
-//Secondo check: l'input utente è assente nel csv
+        if (inputValue.length < 3) {
+            d3.select("p").classed('noresults2', true).html("<center><strong>Parola troppo corta! Digita almeno 3 lettere!</strong>")
+            inputValue = "Something to give no results"
+        }
 
-    if (filteredData.length === 0 && inputValue !== "Something to give no results"){
-      d3.select("p").classed('noresults', true).html("<center><strong>Nessun risultato trovato!</strong>")
-    }
+        //Qui avviene la magia: filtro in base al nome del locale partendo dall'input utente
 
-//Definizione della matrice output giá ordinata dal voto più alto a quello più basso 
+        var filteredData = PostiLodi.filter(PostiLodi => PostiLodi.categories.toLowerCase().trim().includes(inputValue));
 
-    output = _.sortBy(filteredData, 'rating').reverse()
+        //Secondo check: l'input utente è assente nel csv
 
-//Qui viene stampata la tabella finale
+        if (filteredData.length === 0 && inputValue !== "Something to give no results") {
+            d3.select("p").classed('noresults', true).html("<center><strong>Nessun risultato trovato!</strong>")
+        }
 
-    for (var i = 0; i < filteredData.length; i++) {d3.select("tbody").insert("tr").html(
-      "<td>" + (output[i]['name']) + "</td>" +
-      "<td><a href=" + "\"https://maps.google.com?q=" + (output[i]['name']) + "\">" + (output[i]['fulladdr']) + "</a></td>" + 
-      "<td>" + (output[i]['reviews'])+ "</td>" + 
-      "<td>" + (output[i]['rating'])+ "</td>" ) }};
+        //Definizione della matrice output giá ordinata dal voto più alto a quello più basso 
 
-  window.resizeTo(screen.width,screen.height)
+        output = _.sortBy(filteredData, 'rating').reverse()
+
+        for (var i = 0; i < output.length; i++) {
+            if getDistanceFromLatLonInKm(stazioneLodiLatitude, stazioneLodiLongitude, output[i]['latitude'], output[i]['longitude']) <= 1  {
+                console.log(output[i]['name'])
+            }
+        }
+
+        //Qui viene stampata la tabella finale
+
+        for (var i = 0; i < filteredData.length; i++) {
+            d3.select("tbody").insert("tr").html(
+                "<td>" + (output[i]['name']) + "</td>" +
+                "<td><a href=" + "\"https://maps.google.com?q=" + (output[i]['name']) + "\">" + (output[i]['fulladdr']) + "</a></td>" +
+                "<td>" + (output[i]['reviews']) + "</td>" +
+                "<td>" + (output[i]['rating']) + "</td>")
+        }
+    };
+
+    window.resizeTo(screen.width, screen.height)
 
 
 });
--- geolocation.js
+++ geolocation.js
@@ -1,14 +1,32 @@
 const x = document.getElementById("geolocation");
 
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocalizzazione non supportata.";
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocalizzazione non supportata.";
+    }
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitudine: " + position.coords.latitude +
-  "<br>Longitudine: " + position.coords.longitude;
+    x.innerHTML = "Latitudine: " + position.coords.latitude +
+        "<br>Longitudine: " + position.coords.longitude; 
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // raggio Terra
+    var dLat = deg2rad(lat2 - lat1);
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var dis = R * c; // distanza in linea d'aria in Km
+
+    return dis;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
 }
