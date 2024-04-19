17ca87d479828063221642df0edd4fa09b60a859
EdwardBramus <77167194+EdwardBramus@users.noreply.github.com>
2024-04-20 00:26:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ d3.csv("PostiLodi.csv").then(function(data) {
         for (var i = 0; i < filteredData.length; i++) {
             if (getDistanceFromLatLonInKm(lat, lng, output[i]['latitude'], output[i]['longitude']) <= 1000) {
                 d3.select("tbody").style("background-color","yellow").append("tr").html(
-                    "<td style='font-family:Arial, Helvetica, sans-serif;'>" + (output[i]['name']) + "</td>" +
+                    "<td style='font-family:system-ui;'>" + (output[i]['name']) + "</td>" +
                     "<td><a href=" + "\"https://maps.google.com?q=" + (output[i]['name']) + ", " + (output[i]['fulladdr']) + "\">" + (output[i]['fulladdr']) + "</a></td>" +
                     "<td>" + (getDistanceFromLatLonInKm(lat, lng, output[i]['latitude'], output[i]['longitude'])) + "</td>" +
                     "<td>" + (output[i]['reviews']) + "</td>" +
