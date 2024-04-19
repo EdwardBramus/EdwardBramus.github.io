95fc365c8a7d7db8a96d1ecba1f01167fdc96f85
EdwardBramus <77167194+EdwardBramus@users.noreply.github.com>
2024-04-19 21:58:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,41 +6,60 @@ const stazioneLodiLongitude = 9.4976017;
 
 const x = document.getElementById("geolocation");
 
-    d3.csv("PostiLodi.csv").then(function(data) {
+d3.csv("PostiLodi.csv").then(function(data) {
+    
     //Definizione delle variabili
+    
     var PostiLodi = data;
     var button = d3.select("#button");
     var form = d3.select("#form");
+    
     //Interazione dell'utente con gli oggetti che fa partire la funzione runEnter
+    
     button.on("click", runEnter);
     form.on("submit", runEnter);
+    
     //Definizione della funzione runEnter
+    
     function runEnter() {
+        
         d3.select("tbody").html("")
         d3.selectAll("p").classed('noresults', true).html("")
         d3.event.preventDefault();
+        
         //Viene preso in entrata l'input dell'utente e poi ripulito nella riga successiva come nuova variabile...
+        
         var inputElement = d3.select("#user-input");
         var inputValue = inputElement.property("value").toLowerCase().trim();
+        
         //Primo check: l'input dell'utente è troppo breve...
+        
         if (inputValue.length < 3) {
             d3.select("p").classed('noresults2', true).html("<center><strong>Parola troppo corta! Digita almeno 3 lettere!</strong>")
             inputValue = "Something to give no results"
         }
+        
         //Qui avviene la magia: filtro in base al nome del locale partendo dall'input utente
+        
         var filteredData = PostiLodi.filter(PostiLodi => PostiLodi.categories.toLowerCase().trim().includes(inputValue));
+        
         //Secondo check: l'input utente è assente nel csv
+        
         if (filteredData.length === 0 && inputValue !== "Something to give no results") {
             d3.select("p").classed('noresults', true).html("<center><strong>Nessun risultato trovato!</strong>")
         }
+        
         //Definizione della matrice output giá ordinata dal voto più alto a quello più basso 
+        
         output = _.sortBy(filteredData, 'rating').reverse()
         console.log("Posti entro un 1 Km -----")
+        
         for (var i = 0; i < output.length; i++) {
             if (getDistanceFromLatLonInKm(stazioneLodiLatitude, stazioneLodiLongitude, output[i]['latitude'], output[i]['longitude']) <= 1) {
                 console.log(output[i]['name']);
             }
         }
+        
         console.log("----------")
         
         //Qui viene stampata la tabella finale
@@ -58,6 +77,7 @@ const x = document.getElementById("geolocation");
     };
         
     window.resizeTo(screen.width, screen.height)
+
     function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
         var R = 6371; // raggio Terra
         var dLat = deg2rad(lat2 - lat1);
@@ -73,7 +93,7 @@ var dis2 = Math.round(dis * 100) / 100;
         
 return dis2;
     }
+
     function deg2rad(deg) {
         return deg * (Math.PI / 180)
-    }
-});
+    };
