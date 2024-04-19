e09e0e56973a6023846ba8030d57a5ca2d8763db
EdwardBramus <77167194+EdwardBramus@users.noreply.github.com>
2024-04-19 21:20:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,22 +6,6 @@ const stazioneLodiLongitude = 9.4976017;
 
 const x = document.getElementById("geolocation");
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocalizzazione non supportata.";
-    }
-}
-
-function showPosition(position) {
-    getLocation();
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    console.log(lat);
-    console.log(lng);
-    x.innerHTML = "Latitudine: " + lat +
-        "<br>Longitudine: " + lng; 
     d3.csv("PostiLodi.csv").then(function(data) {
     //Definizione delle variabili
     var PostiLodi = data;
